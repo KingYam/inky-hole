a2fc00cf74128926f08984eb10d6fa200d15f38d
Will Foley <folebear@gmail.com>
2023-05-27 11:59:18 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import json
-import urllib2
+import urllib3
+from sys import exit
+
 from inky import InkyPHAT
 from PIL import Image, ImageFont, ImageDraw
 from font_fredoka_one import FredokaOne
@@ -16,20 +18,21 @@ draw = ImageDraw.Draw(img)
 
 # get api data
 
+http = urllib3.PoolManager()
+
 try:
-  f = urllib2.urlopen('http://192.168.0.140/admin/api.php')
-  json_string = f.read()
-  parsed_json = json.loads(json_string)
-  adsblocked = parsed_json['ads_blocked_today']
-  ratioblocked = parsed_json['ads_percentage_today']
-  f.close()
+  r = http.request('GET', 'http://192.168.0.140/admin/api.php')
+  if r.status >= 200 or r.status <= 299:
+    exit("http request is unsuccessful")
 except:
-  queries = '?'
   adsblocked = '?'
-  ratio = '?'
+  ratioblocked = '?'
+
+parsed_json = json.loads(r.data)
+adsblocked = parsed_json['ads_blocked_today']
+ratioblocked = parsed_json['ads_percentage_today']
 
 font = ImageFont.truetype(FredokaOne, 32)
-font_header = ImageFont.truetype(FredokaOne, 16)
 
 inky_display = InkyPHAT("red")
 inky_display.set_border(inky_display.WHITE)
@@ -39,9 +42,9 @@ draw.text((20, 35), str("Ads Blocked"), inky_display.BLACK, font_header)
 draw.text((20,50), str("%.1f" % round(ratioblocked,2)) + "%", inky_display.RED, font)
 draw.text((20,80), str("of all queries"), inky_display.BLACK, font_header)
 
-inky_display.set_image(img)
-
 # inky_display.h_flip = True
 # inky_display.v_flip = True
+
+inky_display.set_image(img)
 
 inky_display.show()
