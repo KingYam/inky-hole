e88e46e7ecc4a47dbaa9ab7749eab89043df0b64
Will Foley <folebear@gmail.com>
2023-05-27 12:46:07 -0700

    Update main.py for proper changes and upgrade

--- main.py
+++ main.py
@@ -20,8 +20,11 @@ draw = ImageDraw.Draw(img)
 
 http = urllib3.PoolManager()
 
+# CHANGE THIS to your URL for your PiHole, and replace <TOKEN HERE> with your API token
+url = 'http://192.168.0.140/admin/api.php?summary&auth=<TOKEN HERE>'
+
 try:
-  r = http.request('GET', 'http://192.168.0.140/admin/api.php')
+  r = http.request('GET', url)
   if r.status >= 200 or r.status <= 299:
     exit("http request is unsuccessful")
 except:
@@ -33,15 +36,22 @@ adsblocked = parsed_json['ads_blocked_today']
 ratioblocked = parsed_json['ads_percentage_today']
 
 font = ImageFont.truetype(FredokaOne, 32)
+font_header = ImageFont.truetype(FredokaOne, 16)
 
 inky_display = InkyPHAT("red")
 inky_display.set_border(inky_display.WHITE)
 
 draw.text((20,5), str(adsblocked), inky_display.RED, font)
 draw.text((20, 35), str("Ads Blocked"), inky_display.BLACK, font_header)
-draw.text((20,50), str("%.1f" % round(ratioblocked,2)) + "%", inky_display.RED, font)
+
+if isinstance(ratioblocked, str):
+  draw.text((20,50), f"{ratioblocked}%", inky_display.RED, font)
+else:
+  draw.text((20,50), f"{ratioblocked:.2f}%", inky_display.RED, font)
+
 draw.text((20,80), str("of all queries"), inky_display.BLACK, font_header)
 
+# For flipping display if mounted
 # inky_display.h_flip = True
 # inky_display.v_flip = True
 
